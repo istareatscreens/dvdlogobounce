97c2585398f6069fe547841377ae70f3f3907eff
istareatscreens <schar138@uottawa.ca>
2020-08-08 15:33:31 -0400

    Added changing image colours on boundary hit

--- src/index.js
+++ src/index.js
@@ -15,29 +15,45 @@ function resizeCanvas() {
 }
 
 function dvdAnimate() {
-  let img = new Image(); // Create new img element
-  img.src = "./images/dvdVideo.png"; // Set source path
+  let img = [new Image(), new Image(), new Image()]; // Create new img element
+  img[0].src = "./images/dvdVideoR.png"; // Set source path
+  img[1].src = "./images/dvdVideoB.png"; // Set source path
+  img[2].src = "./images/dvdVideoG.png"; // Set source path
 
   //Set initial position
-  let x = random(0, ctx.canvas.width - img.width);
-  let y = random(0, ctx.canvas.height - img.height);
+  let x = random(0, ctx.canvas.width - img[0].width);
+  let y = random(0, ctx.canvas.height - img[0].height);
   let vy = 3;
   let vx = 3;
 
   window.requestAnimationFrame(function animateImage() {
+    //calculate screen width and height
+    let selection = 0;
+    let width = ctx.canvas.width - img[selection].width;
+    let height = ctx.canvas.height - img[selection].height;
+    //Velocity
     x += vx;
     y += vy;
-    if ((x >= ctx.canvas.width - img.width && vx > 0) || (x <= 0 && vx < 0)) {
-      vx = -vx;
-    }
-    if ((y >= ctx.canvas.height - img.height && vy > 0) || (y <= 0 && vy < 0)) {
-      vy = -vy;
-    }
+
+    //handle boundary
+    if (0 > (vx = imageBoundry(x, vx, width))) selection++;
+    if (0 > (vy = imageBoundry(y, vy, height))) selection++;
+    if (selection > 2) selection = 0;
+
+    //handle outside boundary
+    if (x > width) x = width;
+    if (y > height) y = height;
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(img, x, y);
+    ctx.drawImage(img[selection], x, y);
     window.requestAnimationFrame(animateImage);
   });
+}
+
+function imageBoundry(axis, velocity, boundary) {
+  return (axis >= boundary && velocity > 0) || (axis <= 0 && velocity < 0)
+    ? -velocity
+    : velocity;
 }
 
 function drawImage(x, y, ctx, img) {
