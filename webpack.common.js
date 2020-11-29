a28b05b37e93af4e5d197aac33ba7a9e6450f286
istareatscreens <schar138@uottawa.ca>
2020-11-29 07:14:18 -0500

    Changed production output path of wasm file

--- webpack.common.js
+++ webpack.common.js
@@ -36,7 +36,7 @@ module.exports = {
                     {
                         loader: "file-loader",
                         options: {
-                            name: "src/wasm/[name].[hash].[ext]",
+                            name: "[name].[hash].[ext]",
                             publicPath: "/dist/"
                         }
                     }
